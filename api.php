8c384ec8157b47eec5d38914a95e6f1b48dd8684
kmj233 <68221518+kmj233@users.noreply.github.com>
2020-07-22 01:21:42 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
 
 $rand = rand(1,201);
-$url = 'https://cdn.jsdelivr.net/gh/kmj233/setu/setu/'.$rand.'.jpg';
+$url = 'https://cdn.jsdelivr.net/gh/dateolive/images@V-2.0/images/'.$rand.'.jpg';
 $img = file_get_contents($url,true);
 header("Content-Type: image/jpeg;");
 echo $img;
