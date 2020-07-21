517da9ee5ebc0c79911c964bde74c7054e821a0c
kmj233 <68221518+kmj233@users.noreply.github.com>
2020-07-22 03:15:36 +0800

    123

--- api.php
+++ api.php
@@ -1,6 +1,6 @@
 <?php
 
-$rand = rand(1,201);
+$rand = rand(1,4);
 $url = 'https://cdn.jsdelivr.net/gh/kmj233/setu@V-2.0/images/'.$rand.'.jpg';
 $img = file_get_contents($url,true);
 header("Content-Type: image/jpeg;");
