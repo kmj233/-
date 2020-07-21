d2135aedb8220cf7cffecf4bedd6cf8ad8abdfd5
kmj233 <68221518+kmj233@users.noreply.github.com>
2020-07-22 03:35:36 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,8 +1,5 @@
 <?php
-
-$rand = rand(1,4);
-$url = 'https://cdn.jsdelivr.net/gh/kmj233/setu@V-2.0/images/'.$rand.'.jpg';
-$img = file_get_contents($url,true);
-header("Content-Type: image/jpeg;");
-echo $img;
+$rand = rand(1,4);
+$url = 'https://cdn.jsdelivr.net/gh/kmj233/setu/images/'.$rand.'.jpg';
+header("Location:".$url);
 ?>
